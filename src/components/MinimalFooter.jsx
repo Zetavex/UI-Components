38ccf6d97df4e1a9ebf61305a8d032552bfb9524
Shaedow2000 <shaedow2000@proton.me>
2026-06-02 13:00:40 +0100

    Create minimal footer component.

---
+++ src/components/MinimalFooter.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router";
+import "./components.css";
+
+export default function MinimalFooter() {
+  return (
+    <footer
+      className={location.pathname === "/login" ? "md:right-0" : "md:left-0"}
+    >
+      <div className="copyright-section">
+        <h2 className="font-logo">Zetavex</h2>
+        <p>© 2026 | All rights reserved.</p>
+      </div>
+      <div className="footer-links-section">
+        <Link to={""} className="secondary-link">
+          Legal
+        </Link>
+        <Link to={""} className="secondary-link">
+          Support
+        </Link>
+      </div>
+    </footer>
+  );
+}
