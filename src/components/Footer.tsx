6891b1ac909ac8731da05a29291214f467ba3613
Shaedow2000 <shaedow2000@proton.me>
2026-06-09 13:57:08 +0100

    Fix footer logo & dark/light mode.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,48 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
-export default function FooterFooter() {
+export default function Footer() {
+  let [darkMode, setDarkMode] = useState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches,
+  );
+
+  const windowObj = window.matchMedia("(prefers-color-scheme: dark)");
+
+  function handleDarkMode(isDark: boolean) {
+    document.documentElement.classList.toggle("dark");
+    setDarkMode(isDark);
+  }
+
+  useEffect(() => {
+    windowObj.addEventListener("change", (e) => handleDarkMode(e.matches));
+
+    return () =>
+      windowObj.removeEventListener("change", (e) => handleDarkMode(e.matches));
+  }, []);
+
   return (
     <footer>
       <div className="copyright-section">
-        <h2 className="font-logo">Zetavex</h2>
+        {darkMode ? (
+          <img
+            src="https://assets.klirond.workers.dev/unicolor-full-logo-light-bg-1.svg"
+            alt="KLIROND logo"
+            width="100"
+          />
+        ) : (
+          <img
+            src="https://assets.klirond.workers.dev/unicolor-full-logo-light-bg.svg"
+            alt="KLIROND logo"
+            width="100"
+          />
+        )}
         <p>© 2026 | All rights reserved.</p>
       </div>
       <div className="footer-links-section">
-        <Link to={""} className="secondary-link">
+        <Link to={"/"} className="secondary-link">
           Legal
         </Link>
-        <Link to={""} className="secondary-link">
+        <Link to={"/"} className="secondary-link">
           Support
         </Link>
       </div>
